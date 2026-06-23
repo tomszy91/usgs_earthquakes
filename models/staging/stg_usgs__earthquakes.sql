f26cae47711bac6516d5f0e7347155faf8dd94a7
tomszy91 <t.szyszlo+GIT@gmail.com>
2026-06-24 00:00:32 +0200

    feature: custom schema macro, source yml and stg model added

---
+++ models/staging/stg_usgs__earthquakes.sql
@@ -0,0 +1,19 @@
+with
+    source as (select * from {{ source("raw", "raw_earthquakes") }}),
+    casted as (
+        select
+            cast(event_id as string) as event_id,
+            cast(event_time as timestamp) as event_time,
+            cast(updated_at as timestamp) as updated_at,
+            cast(magnitude as float64) as magnitude,
+            cast(place as string) as place,
+            cast(longitude as float64) as longitude,
+            cast(latitude as float64) as latitude,
+            cast(depth_km as float64) as depth_km,
+            cast(status as string) as status,
+            cast(ingested_at as timestamp) as ingested_at,
+            cast(raw_json as string) as raw_json,
+        from source
+    )
+
+select * from casted
