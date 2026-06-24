c4adca0e1d07038a15a4641b7a2bbee00c4846a6
tomszy91 <t.szyszlo+GIT@gmail.com>
2026-06-25 00:10:17 +0200

    incremental model added

--- models/intermediate/int_usgs__earthquakes_incremental.sql
+++ models/intermediate/int_usgs__earthquakes_incremental.sql
@@ -1,3 +1,11 @@
+{{ config(
+    materialized='incremental',
+    unique_key = 'event_id',
+    incremental_strategy = 'merge',
+    on_schema_change = 'fail',
+    merge_update_columns = ['event_time', 'updated_at', 'magnitude', 'place', 'longitude', 'latitude', 'depth_km', 'status', 'ingested_at', 'raw_json']
+)}}
+
 with
     source as (
         select
@@ -12,14 +20,9 @@ with
             status,
             ingested_at,
             raw_json
-        from {{ ref("stg_usgs__earthquakes") }}),
-    
-    deduplicated as (
-        select
-            *,
-            row_number() over (partition by event_id order by ingested_at desc) as rn
-        from source
-        where rn = 1
-    )
-    
-select * from deduplicated
+        from {{ ref("int_usgs__earthquakes_deduplicate") }})
+        {% if is_incremental() %}
+            where ingested_at >= (select max(ingested_at) from {{ this }})
+        {% endif %}    
+
+select * from source
