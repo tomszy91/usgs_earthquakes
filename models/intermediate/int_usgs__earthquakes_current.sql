05304393030c18c843cb70088c6e9b2d53ab5315
tomszy91 <t.szyszlo+GIT@gmail.com>
2026-06-25 00:20:56 +0200

    refactor: incremental logic pushed down

--- models/intermediate/int_usgs__earthquakes_current.sql
+++ models/intermediate/int_usgs__earthquakes_current.sql
@@ -1,3 +1,18 @@
+{{ config(
+    materialized='incremental',
+    unique_key = 'event_id',
+    incremental_strategy = 'merge',
+    on_schema_change = 'fail',
+    merge_update_columns = ['event_time', 'updated_at', 'magnitude', 'place', 'longitude', 'latitude', 'depth_km', 'status', 'ingested_at', 'raw_json']
+)}}
+
+{% if execute and is_incremental() %}
+    {% set max_ingested_at_query %}
+        select max(ingested_at) as max_ingested_at from {{ this }}
+    {% endset %}
+    {% set max_ingested_at = run_query(max_ingested_at_query).columns[0].values()[0] %}
+{% endif %}
+
 with
     source as (
         select
@@ -12,7 +27,11 @@ with
             status,
             ingested_at,
             raw_json
-        from {{ ref("stg_usgs__earthquakes") }}),
+        from {{ ref("stg_usgs__earthquakes") }})
+        {% if is_incremental() %}
+            where ingested_at >= timestamp('{{ max_ingested_at }}')
+        {% endif %}    
+        ,
     
     row_numbered as (
         select
