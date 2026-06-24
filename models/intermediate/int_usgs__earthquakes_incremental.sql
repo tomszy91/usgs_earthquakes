c839718d55da8925802db7fd36edeedb761b5143
tomszy91 <t.szyszlo+GIT@gmail.com>
2026-06-24 23:05:17 +0200

    fix: deduplication overlaping event_id

--- models/intermediate/int_usgs__earthquakes_incremental.sql
+++ models/intermediate/int_usgs__earthquakes_incremental.sql
@@ -14,27 +14,11 @@ with
             raw_json
         from {{ ref("stg_usgs__earthquakes") }}),
     
-    row_numbered as (
+    deduplicated as (
         select
             *,
             row_number() over (partition by event_id order by ingested_at desc) as rn
         from source
-    ),
-
-    deduplicated as (
-        select
-            event_id,
-            event_time,
-            updated_at,
-            magnitude,
-            place,
-            longitude,
-            latitude,
-            depth_km,
-            status,
-            ingested_at,
-            raw_json
-        from row_numbered
         where rn = 1
     )
     
